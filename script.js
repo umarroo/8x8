8ce45aab2c258b994d954f4ddc8da1a53fd256d7
umar <umarudy@outlook.co.id>
2021-08-05 17:52:00 +0700

    Add disorder function

--- script.js
+++ script.js
@@ -4,26 +4,41 @@ for (var i = 1; i <= 8; i++) {
   for (var e = 1; e <= 8; e++) a[i][e] = "null";
 }
 
-
-$(document).ready(function() {
-  $("table").click();
-  
+function disorder(){
   var tbl = $('#tbl > tbody > tr').map(function () {
     return $(this).children().map(function ()
     {
         return $(this);
     });
   });
-  for (var i = 0; i <= 7; i++){
-    tbl[i][7-i].addClass("select")
-    tbl[i][i].addClass("select")
+
+  var max = 7
+  var min = 0
+  for (let i = 0; i <= 7; i++) {
+    var rand = Math.floor(Math.random()*(max-min+1)+min);
+    var rand2 = Math.floor(Math.random()*(max-min+1)+min);
+    tbl[i][rand].addClass("select")
+    tbl[rand2][i].addClass("select")
   }
+}
+
+$(document).ready(function() {
+  $("table").click();
+  
+  disorder()
 });
 
 $(".reset").click(function() {
   $(".select").removeClass("select");
   $("table").click();
 });
+
+$(".disorder").click(function(){
+  $(".select").removeClass("select");
+  $("table").click();
+  disorder();
+})
+
 
 $("td").click(function() {
   if ($(this).hasClass("select")) {
@@ -34,7 +49,6 @@ $("td").click(function() {
 });
 
 $("table").click(function() {
-  console.log(a)
   for (var i = 1; i <= 8; i++){
     a[i][1] = 1
   }
